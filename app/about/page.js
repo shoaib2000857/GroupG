11133a413e663e41a263536ccd74dd165ddf0de9
mr.gaurav9554 <mr.gaurav9554@gmail.com>
2024-09-22 07:57:22 +0000

    add some page like about, contact, etc.

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-blue-500 text-center mt-8">
+        This is About Page
+      </h1>
+    </div>
+  );
+}
