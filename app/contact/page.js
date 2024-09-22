fd66f6f28a1e444422f7fcc849bb59fe1994ffa6
SHOAIB SADIQ SALEHMOHAMED <24f2000857@ds.study.iitm.ac.in>
2024-09-22 09:16:18 +0000

    Added a green based theme to the site

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function page() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-blue-500 text-center mt-8">
+    <div className="bg-green-200 text-green-900 min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-green-700 text-center mt-8">
         This is Contact Page
       </h1>
     </div>
--- app/home/page.js
+++ app/home/page.js
@@ -1,8 +1,9 @@
+import { HeroSection } from "../components/hero";
+
 export default function Home() {
-    return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-       <h1>Home Page</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <main className="text-center m-5 p-10 bg-green-200 text-green-900">
+      <HeroSection />
+    </main>
+  );
+}
