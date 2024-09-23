d04cafe5a0072e0eb57649824db74f93f53f14b8
mr.gaurav9554 <mr.gaurav9554@gmail.com>
2024-09-23 22:52:43 +0000

    connect API to frontend

--- components/hero.js
+++ components/hero.js
@@ -1,21 +1,65 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
+import axios from "axios";
 
 export function HeroSection() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [response, setResponse] = useState('')
+  const [error, setError] = useState(null)
+  const [data, setData] = useState({ prompt: '', type: '' })
+
+  const postData = (event) => setData({ ...data, [event.target.name]: event.target.value })
+  const onSubmit = async (event) => {
+    try {
+      setIsLoading(true)
+      setError(null)
+      setResponse(null)
+      event.preventDefault()
+      const result = await axios.post('/api/gemini', { ...data })
+      console.log(result);
+      if (result.data.success) {
+        setResponse(await result.data.data)
+      } else {
+        setError(result.data.message || "Opps! Something went wrong!")
+      }
+
+      setData({ prompt: '', type: '' })
+
+    } catch (error) {
+      setError(error.message || "Something went wrong!")
+    } finally {
+      setIsLoading(false)
+    }
+  }
   return (
     <div className="bg-green-200 text-green-900 p-8">
       <h1 className="text-2xl font-bold text-green-700">
         Gen AI Exchange Hackathon by Google
       </h1>
-      <form className="mt-2">
+      {response && <p className="text-xl font-bold text-green-500">{response}</p>}
+      {error && <p className="text-red-600">{error}</p>}
+      <form onSubmit={onSubmit} className="mt-2">
+
         <input
           type="text"
-          placeholder="Enter a Text"
-          name="promt"
+          required
+          autoFocus
+          readOnly={isLoading}
+          name="prompt"
+          value={data.prompt}
+          onChange={postData}
+          placeholder="Enter Prompt Here..."
           className="bg-green-100 text-green-900 p-2 rounded"
         />
+        <select name='type' disabled={isLoading} onChange={postData} className="bg-green-100 text-green-900 p-2 rounded">
+          <option value='text'>Text</option>
+          <option value='image'>Image</option>
+          <option value='video'>Video</option>
+        </select>
         <br />
-        <button className="btn btn-primary bg-green-500 hover:bg-green-300 rounded m-1 p-2">
-          Submit
+        <button disabled={isLoading}
+          className="btn btn-primary bg-green-500 hover:bg-green-300 rounded m-1 p-2">
+          {isLoading ? 'Loading...' : 'Submit'}
         </button>
       </form>
     </div>
