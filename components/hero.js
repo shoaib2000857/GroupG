31a7d22af0a34344b7aabd1fdb1b66ce92020904
SHOAIB SADIQ SALEHMOHAMED <24f2000857@ds.study.iitm.ac.in>
2024-09-25 19:48:45 +0530

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -38,30 +38,6 @@ export function HeroSection() {
       </h1>
       {response && <p className="text-xl font-bold text-green-500">{response}</p>}
       {error && <p className="text-red-600">{error}</p>}
-      <form onSubmit={onSubmit} className="mt-2">
-
-        <input
-          type="text"
-          required
-          autoFocus
-          readOnly={isLoading}
-          name="prompt"
-          value={data.prompt}
-          onChange={postData}
-          placeholder="Enter Prompt Here..."
-          className="bg-green-100 text-green-900 p-2 rounded"
-        />
-        <select name='type' disabled={isLoading} onChange={postData} className="bg-green-100 text-green-900 p-2 rounded">
-          <option value='text'>Text</option>
-          <option value='image'>Image</option>
-          <option value='video'>Video</option>
-        </select>
-        <br />
-        <button disabled={isLoading}
-          className="btn btn-primary bg-green-500 hover:bg-green-300 rounded m-1 p-2">
-          {isLoading ? 'Loading...' : 'Submit'}
-        </button>
-      </form>
     </div>
   );
 }
